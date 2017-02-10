c3ffe6307a16008be29dfb4aabfc268eae1668b6
lewhi7273 <lewhi7273@7KXL6Y1.Student.UGDSB.ED>
2017-02-10 13:52:41 -0500

    This is my first hippity change for helloworld

---
+++ src/helloworld/Helloworld.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package helloworld;
+
+/**
+ *
+ * @author lewhi7273
+ */
+public class Helloworld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hallo Jimminy Crickets");
+        System.out.println("I am your mother and father at the same time");
+        System.out.println("Hippity Hoppitus do the flopitus");
+    }
+    
+}
